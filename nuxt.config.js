ee1a68a98d635bd7469c2fd71073e9aa6f9bda03
iRuxu <rx6@qq.com>
2022-08-09 02:40:24 +0800

    feat:config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,37 @@
+import {description} from './project.json'
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'easticloud-www',
-    htmlAttrs: {
-      lang: 'en'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: "东云科技",
+        htmlAttrs: {
+            lang: "zh-CN",
+        },
+        meta: [
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+            { hid: "description", name: "description", content: description },
+            { name: "format-detection", content: "telephone=no" },
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: ["element-ui/lib/theme-chalk/index.css",'csslab/reset.css'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/element-ui'
-  ],
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: ["@/plugins/element-ui"],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    transpile: [/^element-ui/],
-  }
-}
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        transpile: [/^element-ui/],
+    },
+};
