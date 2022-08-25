1f867149e21852a50f7350a3aa6812810732dabd
litthy <litthy@qq.com>
2022-08-25 15:42:16 +0800

    fix: 百度地图 #8

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -30,7 +30,18 @@
                     </div>
                 </div>
                 <div class="m-map">
-                    <img class="u-img" src="../../static/images/index/contact/map.png" alt="江苏东云互联网络科技有限公司" />
+                    <baidu-map class="u-map" :center="center" :zoom="zoom" ak="Ew6p8zLR26m3UX3cu4ibtwUGqwprCmZI">
+                        <baidu-map-marker :position="{ lng: 118.726879, lat: 31.997976 }"></baidu-map-marker>
+                        <BaiduMapOverlay pane="labelPane" class="m-address" @draw="draw">
+                            <div class="u-logo">
+                                <img src="../../static/images/index/contact/logo.svg" alt="江苏东云互联网络科技有限公司">
+                            </div>
+                            <div class="u-info">
+                                <span class="u-title">江苏东云互联网络科技有限公司</span>
+                                <span>江苏省南京市建邺区江东中路359号国睿大厦1号楼B座16层</span>
+                            </div>
+                        </BaiduMapOverlay>
+                    </baidu-map>
                 </div>
             </div>
             <div class="m-info-box" id="tab2">
@@ -91,8 +102,16 @@
 </template>
 <script>
 import { createMessage } from "@/utils/api.js";
+import BaiduMap from 'vue-baidu-map/components/map/Map.vue'
+import BaiduMapMarker from 'vue-baidu-map/components/overlays/Marker.vue'
+import BaiduMapOverlay from 'vue-baidu-map/components/overlays/Overlay.vue'
 export default {
     name: "ContactPage",
+    components: {
+        BaiduMap,
+        BaiduMapMarker,
+        BaiduMapOverlay
+    },
     data: function () {
         return {
             form: {
@@ -125,6 +144,9 @@ export default {
                 },
             },
             active: "",
+
+            center: { lng: '118.726879', lat: '31.997976' },
+            zoom: 16,
         };
     },
     watch: {},
@@ -182,7 +204,7 @@ export default {
 
         onSubmit () {
             this.checkForm(this.form)
-            const isSubmit = Object.values(this.rules).map(item=>item.isErr).filter(Boolean);
+            const isSubmit = Object.values(this.rules).map(item => item.isErr).filter(Boolean);
             console.log(isSubmit)
             !isSubmit.length &&
                 createMessage(this.form).then(() => {
@@ -194,6 +216,12 @@ export default {
                     this.onReset();
                 });
         },
+        // 地图标签定位
+        draw ({ el, BMap, map }) {
+            const pixel = map.pointToOverlayPixel(new BMap.Point(118.726679, 31.998076))
+            el.style.left = pixel.x - 124 + 'px'
+            el.style.top = pixel.y - 142 + 'px'
+        }
     },
 };
 </script>
